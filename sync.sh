7f4151d8cdd936a2e5ccb2fd028c89a7f26f0f4a
Matthew Austin <matt.austin94@gmail.com>
2016-03-12 16:19:35 -0800

    update sync script

--- sync.sh
+++ sync.sh
@@ -1,2 +1,2 @@
-rsync -avz --rsh="ssh -i /Users/matthewaustin/Desktop/MattsKey.pem" "/Users/matthewaustin/Documents/chaos-node/" bitnami@54.191.247.59:chaos
+rsync -avz --exclude 'data' --rsh="ssh -i /Users/matthewaustin/Desktop/MattsKey.pem" "/Users/matthewaustin/Documents/chaos-node/" bitnami@54.191.247.59:chaos
 
